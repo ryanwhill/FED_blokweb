704e1805955db4316b6cf2a03cd4e47d70c22f5b
ryanwhill <118119324+ryanwhill@users.noreply.github.com>
2022-12-04 16:33:32 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("button:first-of-type");
+var navUl = document.querySelector("nav ul");
+
+menuButton.addEventListener("click", openMenu);
+
+function openMenu(){
+    navUl.classList.toggle("toonMenu");
+}
+
